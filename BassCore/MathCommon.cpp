935d74ad74d53df36b74216168942c729875972b
BassGoon <garshion@gmail.com>
2020-07-31 02:19:10 +0900

    단위 관련 적용. 길이쪽은 변환함수 구현됨.

--- BassCore/MathCommon.cpp
+++ BassCore/MathCommon.cpp
@@ -24,58 +24,6 @@ bool Bass::IsZero(const float& val, const float& tol) noexcept
 	return IsSame(val, tol);
 }
 
-double Bass::RadianToDegree(const double& rad)
-{
-	return rad * PI_DEGREE * DIV_PI;
-}
-
-double Bass::DegreeToRadian(const double& deg)
-{
-	return deg * PI * DIV_PI_DEGREE;
-}
-
-float Bass::RadianToDegree(const float& rad)
-{
-	return rad * PI_DEGREE_32 * DIV_PI_32;
-}
-
-float Bass::DegreeToRadian(const float& deg)
-{
-	return deg * PI_32 * DIV_PI_DEGREE_32;
-}
-
-double Bass::DegreeNormalize(const double& deg)
-{
-	double dRet = fmod(deg, PI2_DEGREE);
-	if (dRet < 0.0)
-		dRet += PI2_DEGREE;
-	return dRet;
-}
-
-float Bass::DegreeNormalize(const float& deg)
-{
-	float fRet = fmodf(deg, PI2_DEGREE_32);
-	if (fRet < 0.0f)
-		fRet += PI2_DEGREE_32;
-	return fRet;
-}
-
-double Bass::RadianNormalize(const double& rad)
-{
-	double dRet = fmod(rad, PI2);
-	if (dRet < 0.0)
-		dRet += PI2;
-	return dRet;
-}
-
-float Bass::RadianNormalize(const float& rad)
-{
-	float fRet = fmodf(rad, PI2_32);
-	if (fRet < 0.0f)
-		fRet += PI2_32;
-	return fRet;
-}
-
 bool Bass::IsValid(const double& val)
 {
 	if (true == isnan(val)
@@ -90,4 +38,266 @@ bool Bass::IsValid(const float& val)
 		|| true == isinf(val))
 		return false;
 	return true;
+}
+
+
+
+Bass::Degree Bass::RadianToDegree(const Bass::Radian& rad)
+{
+	return rad * PI_DEGREE * DIV_PI;
+}
+
+Bass::Radian Bass::DegreeToRadian(const Bass::Degree& deg)
+{
+	return deg * PI * DIV_PI_DEGREE;
+}
+
+Bass::Degree32 Bass::RadianToDegree(const Bass::Radian32& rad)
+{
+	return rad * PI_DEGREE_32 * DIV_PI_32;
+}
+
+Bass::Radian32 Bass::DegreeToRadian(const Bass::Degree32& deg)
+{
+	return deg * PI_32 * DIV_PI_DEGREE_32;
+}
+
+Bass::Degree Bass::DegreeNormalize(const Bass::Degree& deg)
+{
+	double dRet = fmod(deg, PI2_DEGREE);
+	if (dRet < 0.0)
+		dRet += PI2_DEGREE;
+	return dRet;
+}
+
+Bass::Degree32 Bass::DegreeNormalize(const Bass::Degree32& deg)
+{
+	float fRet = fmodf(deg, PI2_DEGREE_32);
+	if (fRet < 0.0f)
+		fRet += PI2_DEGREE_32;
+	return fRet;
+}
+
+Bass::Radian Bass::RadianNormalize(const Bass::Radian& rad)
+{
+	double dRet = fmod(rad, PI2);
+	if (dRet < 0.0)
+		dRet += PI2;
+	return dRet;
+}
+
+Bass::Radian32 Bass::RadianNormalize(const Bass::Radian32& rad)
+{
+	float fRet = fmodf(rad, PI2_32);
+	if (fRet < 0.0f)
+		fRet += PI2_32;
+	return fRet;
+}
+
+Bass::Meter Bass::ToMeter(const double& val, const EDistance& inputType)
+{
+	switch (inputType)
+	{
+	case EDistance::Meter: return val;
+	case EDistance::CentiMeter: return val * 0.01;
+	case EDistance::MilliMeter: return val * 0.001;
+	case EDistance::KiloMeter: return val * 1000.0;
+	case EDistance::Feet: return val * 0.3048;
+	case EDistance::FlightLevel: return val * 30.48;
+	case EDistance::Mile: return val * 1609.34;
+	case EDistance::NauticalMile: return val * 1852.0;
+	case EDistance::Inch: return val * 0.0254;
+	case EDistance::Yard: return val * 0.9144;
+	default:
+		break;
+	}
+
+	return Meter(0);
+}
+
+Bass::CentiMeter Bass::ToCentiMeter(const double& val, const EDistance& inputType)
+{
+	switch (inputType)
+	{
+	case EDistance::Meter: return val * 100.0;
+	case EDistance::CentiMeter: return val;
+	case EDistance::MilliMeter: return val * 0.1;
+	case EDistance::KiloMeter: return val * 100000.0;
+	case EDistance::Feet: return val * 30.48;
+	case EDistance::FlightLevel: return val * 3048.0;
+	case EDistance::Mile: return val * 160934.0;
+	case EDistance::NauticalMile: return val * 185200.0;
+	case EDistance::Inch: return val * 2.54;
+	case EDistance::Yard: return val * 91.44;
+	default:
+		break;
+	}
+
+	return CentiMeter(0);
+}
+
+Bass::MilliMeter Bass::ToMilliMeter(const double& val, const EDistance& inputType)
+{
+	switch (inputType)
+	{
+	case EDistance::Meter: return val * 1000.0;
+	case EDistance::CentiMeter: return val * 10.0;
+	case EDistance::MilliMeter: return val;
+	case EDistance::KiloMeter: return val * 1000000.0;
+	case EDistance::Feet: return val * 304.8;
+	case EDistance::FlightLevel: return val * 30480.0;
+	case EDistance::Mile: return val * 1609340.0;
+	case EDistance::NauticalMile: return val * 1852000.0;
+	case EDistance::Inch: return val * 25.4;
+	case EDistance::Yard: return val * 914.4;
+	default:
+		break;
+	}
+
+	return MilliMeter(0);
+}
+
+Bass::KiloMeter Bass::ToKiloMeter(const double& val, const EDistance& inputType)
+{
+	switch (inputType)
+	{
+	case EDistance::Meter: return val * 0.001;
+	case EDistance::CentiMeter: return val * 0.00001;
+	case EDistance::MilliMeter: return val * 0.000001;
+	case EDistance::KiloMeter: return val;
+	case EDistance::Feet: return val * 0.0003048;
+	case EDistance::FlightLevel: return val * 0.03048;
+	case EDistance::Mile: return val * 1.60934;
+	case EDistance::NauticalMile: return val * 1.852;
+	case EDistance::Inch: return val * 0.0000254;
+	case EDistance::Yard: return val * 0.0009144;
+	default:
+		break;
+	}
+
+	return KiloMeter(0);
+}
+
+Bass::Feet Bass::ToFeet(const double& val, const EDistance& inputType)
+{
+	switch (inputType)
+	{
+	case EDistance::Meter: return val * 3.28084;
+	case EDistance::CentiMeter: return val * 0.0328084;
+	case EDistance::MilliMeter: return val * 0.00328084;
+	case EDistance::KiloMeter: return val * 3280.84;
+	case EDistance::Feet: return val;
+	case EDistance::FlightLevel: return val * 100.0;
+	case EDistance::Mile: return val * 5280.0;
+	case EDistance::NauticalMile: return val * 6076.12;
+	case EDistance::Inch: return val * 0.0833333;
+	case EDistance::Yard: return val * 3.0;
+	default:
+		break;
+	}
+
+	return Feet(0);
+}
+
+Bass::FlightLevel Bass::ToFL(const double& val, const EDistance& inputType)
+{
+	switch (inputType)
+	{
+	case EDistance::Meter: return val * 0.0328084;
+	case EDistance::CentiMeter: return val * 0.000328084;
+	case EDistance::MilliMeter: return val * 0.0000328084;
+	case EDistance::KiloMeter: return val * 32.8084;
+	case EDistance::Feet: return val * 0.01;
+	case EDistance::FlightLevel: return val;
+	case EDistance::Mile: return val * 52.80;
+	case EDistance::NauticalMile: return val * 60.7612;
+	case EDistance::Inch: return val * 0.000833333;
+	case EDistance::Yard: return val * 0.03;
+	default:
+		break;
+	}
+
+	return FlightLevel(0);
+}
+
+Bass::Mile Bass::ToMile(const double& val, const EDistance& inputType)
+{
+	switch (inputType)
+	{
+	case EDistance::Meter: return val / 1609.34;
+	case EDistance::CentiMeter: return val / 160934.0;
+	case EDistance::MilliMeter: return val / 1609340.0;
+	case EDistance::KiloMeter: return val / 1.60934;
+	case EDistance::Feet: return val / 5280;
+	case EDistance::FlightLevel: return val / 52.80;
+	case EDistance::Mile: return val;
+	case EDistance::NauticalMile: return val * 1.15078;
+	case EDistance::Inch: return val / 63360.0;
+	case EDistance::Yard: return val / 1760.0;
+	default:
+		break;
+	}
+
+	return Mile(0);
+}
+
+Bass::NauticalMile Bass::ToKN(const double& val, const EDistance& inputType)
+{
+	switch (inputType)
+	{
+	case EDistance::Meter: return val / 1852.0;
+	case EDistance::CentiMeter: return val / 185200.0;
+	case EDistance::MilliMeter: return val / 1852000.0;
+	case EDistance::KiloMeter: return val / 1.852;
+	case EDistance::Feet: return val / 6076.12;
+	case EDistance::FlightLevel: return val / 60.7612;
+	case EDistance::Mile: return val * 0.868976;
+	case EDistance::NauticalMile: return val;
+	case EDistance::Inch: return val / 72913.4;
+	case EDistance::Yard: return val / 2025.37;
+	default:
+		break;
+	}
+
+	return NauticalMile(0);
+}
+
+Bass::Inch Bass::ToInch(const double& val, const EDistance& inputType)
+{
+	switch (inputType)
+	{
+	case EDistance::Meter: return val * 39.3701;
+	case EDistance::CentiMeter: return val * 0.393701;
+	case EDistance::MilliMeter: return val * 0.0393701;
+	case EDistance::KiloMeter: return val * 39370.1;
+	case EDistance::Feet: return val * 12.0;
+	case EDistance::FlightLevel: return val * 1200.0;
+	case EDistance::Mile: return val * 63360.0;
+	case EDistance::NauticalMile: return val * 72913.4;
+	case EDistance::Inch: return val;
+	case EDistance::Yard: return val * 36.0;
+	default:
+		break;
+	}
+	return Inch(0);
+}
+
+Bass::Yard Bass::ToYard(const double& val, const EDistance& inputType)
+{
+	switch (inputType)
+	{
+	case EDistance::Meter: return val * 1.09361;
+	case EDistance::CentiMeter: return val * 0.0109361;
+	case EDistance::MilliMeter: return val * 0.00109361;
+	case EDistance::KiloMeter: return val * 1093.61;
+	case EDistance::Feet: return val / 3.0;
+	case EDistance::FlightLevel: return val * 100.0 / 3.0;
+	case EDistance::Mile: return val * 1760.0;
+	case EDistance::NauticalMile: return val * 2025.37;
+	case EDistance::Inch: return val / 36.0;
+	case EDistance::Yard: return val;
+	default:
+		break;
+	}
+	return Yard(0);
 }
--- BassCore/MathCommon.h
+++ BassCore/MathCommon.h
@@ -3,23 +3,9 @@
 
 namespace Bass
 {
-
-	// 원주율 관련
-	constexpr double PI = 3.14159265358979323846264338327950288419716939937510;			// 배정밀도 원주율
-	constexpr float PI_32 = 3.1415927f;	// 단정밀도 원주율
-	constexpr double DIV_PI = 1.0 / PI;
-	constexpr float DIV_PI_32 = 1.0f / PI_32;
-
-	constexpr double PI2 = PI * 2.0;
-	constexpr float PI2_32 = PI_32 * 2.0f;
-
-	constexpr double PI_DEGREE = 180.0;
-	constexpr double PI2_DEGREE = 360.0;
-	constexpr double DIV_PI_DEGREE = 1.0 / 180.0;
-
-	constexpr float PI_DEGREE_32 = 180.0f;
-	constexpr float PI2_DEGREE_32 = 360.0f;
-	constexpr float DIV_PI_DEGREE_32 = 1.0f / 180.0f;
+	////////////////////////////////////////////////////////////////////////////////
+	// 공통
+	////////////////////////////////////////////////////////////////////////////////
 
 	// 근사 오차값
 	constexpr double TOLORANCE = 0.000000000000001;
@@ -35,16 +21,116 @@ namespace Bass
 	bool IsZero(const double& val, const double& tol = TOLORANCE) noexcept;
 	bool IsZero(const float& val, const float& tol = TOLORANCE_32) noexcept;
 
-	double RadianToDegree(const double& rad);
-	double DegreeToRadian(const double& deg);
-	float RadianToDegree(const float& rad);
-	float DegreeToRadian(const float& deg);
-
-	double DegreeNormalize(const double& deg);	// 0~359
-	float DegreeNormalize(const float& deg);
-	double RadianNormalize(const double& rad);
-	float RadianNormalize(const float& rad);
 
 	bool IsValid(const double& val);
 	bool IsValid(const float& val);
+
+
+	////////////////////////////////////////////////////////////////////////////////
+	// 삼각함수 관련
+	////////////////////////////////////////////////////////////////////////////////
+
+	using Radian = double;
+	using Degree = double;
+	using Radian32 = float;
+	using Degree32 = float;
+
+	constexpr double PI = 3.14159265358979323846264338327950288419716939937510;			// 배정밀도 원주율
+	constexpr float PI_32 = 3.1415927f;	// 단정밀도 원주율
+	constexpr double DIV_PI = 1.0 / PI;
+	constexpr float DIV_PI_32 = 1.0f / PI_32;
+
+	constexpr double PI2 = PI * 2.0;
+	constexpr float PI2_32 = PI_32 * 2.0f;
+
+	constexpr Degree PI_DEGREE = 180.0;
+	constexpr Degree PI2_DEGREE = 360.0;
+	constexpr double DIV_PI_DEGREE = 1.0 / 180.0;
+
+	constexpr Degree32 PI_DEGREE_32 = 180.0f;
+	constexpr Degree32 PI2_DEGREE_32 = 360.0f;
+	constexpr float DIV_PI_DEGREE_32 = 1.0f / 180.0f;
+
+	Degree RadianToDegree(const Radian& rad);
+	Radian DegreeToRadian(const Degree& deg);
+	Degree32 RadianToDegree(const Radian32& rad);
+	Radian32 DegreeToRadian(const Degree32& deg);
+
+	Degree DegreeNormalize(const Degree& deg);	// 0~359
+	Degree32 DegreeNormalize(const Degree32& deg);
+	Radian RadianNormalize(const Radian& rad);
+	Radian32 RadianNormalize(const Radian32& rad);
+
+
+
+	////////////////////////////////////////////////////////////////////////////////
+	// 거리 관련
+	////////////////////////////////////////////////////////////////////////////////
+
+	enum class EDistance : int
+	{	
+		Meter,				
+		CentiMeter,
+		MilliMeter,
+		KiloMeter,
+		Feet,
+		FlightLevel,
+		Mile,
+		NauticalMile,
+		Inch,
+		Yard,
+	};
+
+	using Meter = double;
+	using CentiMeter = double;
+	using MilliMeter = double;
+	using KiloMeter = double;
+	using Feet = double;
+	using FlightLevel = double;
+	using Mile = double;
+	using NauticalMile = double;	// KN
+	using Inch = double;
+	using Yard = double;
+
+	Meter ToMeter(const double& val, const EDistance& inputType);
+	CentiMeter ToCentiMeter(const double& val, const EDistance& inputType);
+	MilliMeter ToMilliMeter(const double& val, const EDistance& inputType);
+	KiloMeter ToKiloMeter(const double& val, const EDistance& inputType);
+	Feet ToFeet(const double& val, const EDistance& inputType);
+	FlightLevel ToFL(const double& val, const EDistance& inputType);
+	Mile ToMile(const double& val, const EDistance& inputType);
+	NauticalMile ToKN(const double& val, const EDistance& inputType);
+	Inch ToInch(const double& val, const EDistance& inputType);
+	Yard ToYard(const double& val, const EDistance& inputType);
+
+
+
+
+	////////////////////////////////////////////////////////////////////////////////
+	// 속도 관련
+	////////////////////////////////////////////////////////////////////////////////
+
+	// Speed
+	using Knot = double;
+	using kmh = double;
+	using ms = double;
+	using Mach = double;
+
+
+	////////////////////////////////////////////////////////////////////////////////
+	// 온도 관련
+	////////////////////////////////////////////////////////////////////////////////
+
+	// Temperature
+	using Celcius = double;		// 섭씨
+	using Kelvin = double;		// 절대온도
+	using Fahrenheit = double;	// 화씨
+
+	////////////////////////////////////////////////////////////////////////////////
+	// 무게 관련
+	////////////////////////////////////////////////////////////////////////////////
+
+
+
+
 }
