06fe73e587dfe876abcc91fd0b4b9dae170c41fd
jmyoon <garshion@gmail.com>
2020-08-09 03:09:39 +0900

    주석 처리

--- BassCore/LogManager.h
+++ BassCore/LogManager.h
@@ -4,13 +4,13 @@
 
 namespace Bass
 {
-	constexpr const wchar_t* DEFAULT_LOG_PATH = L"./Log/";		// 기본 로그 폴더 경로
-	constexpr const wchar_t* DEFAULT_LOG_FILENAME = L"System.log";	// 기본 로그 파일명
+	constexpr const wchar_t* DEFAULT_LOG_PATH = L"./Log/";					// 기본 로그 폴더 경로
+	constexpr const wchar_t* DEFAULT_LOG_FILENAME = L"System.log";			// 기본 로그 파일명
 
 	constexpr size_t DEFAULT_LOG_FILE_MAX_SIZE = 10 * 1024 * 1024;			// 로그파일 최대 크기 (분할)
 	constexpr int DEFAULT_LOG_WRITE_TICK_DELAY_MS = 50;						// 로그 Thread sleep 시간
 
-	constexpr size_t VARIABLE_FORMAT_BUFFER_SIZE = 1024;
+	constexpr size_t VARIABLE_FORMAT_BUFFER_SIZE = 1024;					// 가변인자 버퍼 크기
 
 	class LogManagerImpl;
 	class LogManager : public Singleton<LogManager>
